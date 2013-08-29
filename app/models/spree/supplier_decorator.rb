17635ddc69ca7a7858b2ba7c4cfb7f68c6b4e0a0
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-08-29 01:38:30 -0400

    Fix balanced update.

--- app/models/spree/supplier_decorator.rb
+++ app/models/spree/supplier_decorator.rb
@@ -32,9 +32,9 @@ Spree::Supplier.class_eval do
       customer.attributes['address']['line1'] = self.address.try(:address1)
       customer.attributes['address']['line2'] = self.address.try(:address2)
       customer.attributes['address']['city'] = self.address.try(:city)
-      customer.attributes['address']['state'] = (self.address.state ? self.address.state.name : nil)
+      customer.attributes['address']['state'] = (self.address.try(:state) ? self.address.state.name : nil)
       customer.attributes['address']['postal_code'] = self.address.try(:zipcode)
-      customer.attributes['address']['country_code'] = (self.address.country ? self.address.country.iso : nil)
+      customer.attributes['address']['country_code'] = (self.address.try(:country) ? self.address.country.iso : nil)
       customer.save
     end
     return true # always return true so AR saving continues even if customer.save fails.
