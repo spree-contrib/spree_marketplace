a7a279e5d20978a9d91dead5654798230cf21706
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-23 01:50:54 -0400

    Fix gemfile.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ group :development, :test do
   spree_branch = '2-3-stable'
   gem 'spree',                  github: 'spree/spree',                  branch: spree_branch
   gem 'spree_auth_devise',      github: 'spree/spree_auth_devise',      branch: spree_branch
-  gem 'spree_drop_ship',path:'../spree_drop_ship'#        github: 'jdutil/spree_drop_ship',       branch: spree_branch
+  gem 'spree_drop_ship',        github: 'jdutil/spree_drop_ship',       branch: spree_branch
   gem 'spree_digital',          github: 'halo/spree_digital',           branch: spree_branch
   gem 'spree_editor',           github: 'spree/spree_editor',           branch: spree_branch
   gem 'spree_group_pricing',    github: 'jdutil/spree_group_pricing',   branch: spree_branch
--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -10,32 +10,4 @@ Spree::Admin::ProductsController.class_eval do
       end
     end
 
-    # def collection
-    #   return parent.send(controller_name) if parent_data.present?
-    #   if model_class.respond_to?(:accessible_by) && !current_ability.has_block?(params[:action], model_class)
-    #     model_class.accessible_by(current_ability, action)
-    #   else
-    #     model_class.scoped
-    #   end
-    # end
-
-    # def collection
-    #   return @collection if @collection.present?
-    #   params[:q] ||= {}
-    #   params[:q][:deleted_at_null] ||= "1"
-    #
-    #   params[:q][:s] ||= "name asc"
-    #   @collection = super
-    #   # @collection = @collection.with_deleted if params[:q][:deleted_at_null] == '0'
-    #   # @search needs to be defined as this is passed to search_form_for
-    #   @search = @collection.ransack(params[:q])
-    #   @collection = @search.result.
-    #         distinct_by_product_ids(params[:q][:s]).
-    #         includes(product_includes).
-    #         page(params[:page]).
-    #         per(Spree::Config[:admin_products_per_page])
-    #
-    #   @collection
-    # end
-
 end
